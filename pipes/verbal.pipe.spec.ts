29353082b9b7d1ef35706d2bef0cc04735a372a7
TanguturuSucharitha <60088057+TanguturuSucharitha@users.noreply.github.com>
2020-03-02 21:32:21 -0800

    del

--- pipes/verbal.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VerbalPipe } from './verbal.pipe';
-
-describe('VerbalPipe', () => {
-  it('create an instance', () => {
-    const pipe = new VerbalPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
